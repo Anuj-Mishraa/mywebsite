7543f5eea764268312b94f5599f4142506f8ad93
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-19 00:07:40 +0530

    logotiyle

--- index.py
+++ index.py
@@ -2,7 +2,34 @@
 import streamlit as st
 from PIL import Image
 import time
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide",page_title="Anuj Mishra",page_icon="🧑‍🎓")
+
+# def set_page_title(title):
+#     st.markdown(unsafe_allow_html=True, body=f"""
+#         <iframe height=0 srcdoc="<script>
+#             const title = window.parent.document.querySelector('title') \
+                
+#             const oldObserver = window.parent.titleObserver
+#             if (oldObserver) {{
+#                 oldObserver.disconnect()
+#             }} \
+
+#             const newObserver = new MutationObserver(function(mutations) {{
+#                 const target = mutations[0].target
+#                 if (target.text !== '{title}') {{
+#                     target.text = '{title}'
+#                 }}
+#             }}) \
+
+#             newObserver.observe(title, {{ childList: true }})
+#             window.parent.titleObserver = newObserver \
+
+#             title.text = '{title}'
+#         </script>" />
+#     """)
+
+
+# set_page_title("Anuj Mishra")
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
