e013b27654cb2cfa699720d8e314fb535f1394d5
Anuj Mishra <anujmishra282003@gmail.com>
2023-02-19 00:20:14 +0530

    i

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from PIL import Image
 import time
-st.set_page_config(layout="wide",page_title="Anuj Mishra",page_icon="random")
+st.set_page_config(layout="wide",page_title="Anuj Mishra",page_icon="🧊")
 
 # def set_page_title(title):
 #     st.markdown(unsafe_allow_html=True, body=f"""
